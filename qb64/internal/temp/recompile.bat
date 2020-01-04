ad5d4b9194685d823cb67591cc961f684b97ec36
Steve McNeill <smcneill@swva.net>
2020-01-04 10:24:37 -0500

    Folder Clean Up
    Cleaning up the main folder and such so that it's not such a cluttered mess to begin with.  ALL the required SDL files are moved into their own subdirectory, since I have an exact copy of these files included in my OS PATH.  QB64 has no problems finding them for me, and I don't need them cluttering up the folder.
    
    They're left here, in case someone else may need them to copy/move to the QB64 main directory (or into a valid PATH where QB64 can locate them).

--- qb64/internal/temp/recompile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd %0\..\
-echo Recompiling...
-cd ../c
-.\bin\g -fstack-check -w -Wall -I.\i -I..\..\ -L.\l qbx.cpp libqbx.o -mconsole -mwindows .\i686-w64-mingw32\lib\libimm32.a -lwinspool -lmingw32 -ln -lmix -limg -lttf -lSDL -s -o "..\..\qb64(2).exe"
-pause
